aa31d594de4c579cf2de396654f4c8a2dcf0ed89
Krivchenko Sergei <sergey.serj.serj@gmail.com>
2020-08-07 16:55:37 +0300

    Coding style applied

--- rtl/serializer.sv
+++ rtl/serializer.sv
@@ -1,86 +1,55 @@
-module serializer(
+module serializer (
+  input                 clk_i,
+  input                 srst_i,
 
-	// Тактовый сигнал
-	input 			clk_i,
+  input                 data_val_i,
+  input         [3:0]   data_mod_i,
+  input         [15:0]  data_i,
 
-	// Синхронный сброс
-	input 			srst_i,
+  output logic          ser_data_o,
+  output logic          ser_data_val_o,
 
-	// Входные данные
-	input 			data_val_i,
-	input [3:0] 	data_mod_i,
-	input [15:0] 	data_i,
-
-	// Выходные данные
-	output logic 	ser_data_o,
-	output logic 	ser_data_val_o,
-	output logic 	busy_o
+  output logic          busy_o
 );
 
-//-----------------------------------------
-//	Variables declaration
-//-----------------------------------------
+logic [15:0] ser_data;
+logic [3:0]  ser_data_cnt;
 
-	logic [15:0] ser_data;
-	logic [3:0]  ser_data_cnt;
-	logic 		 valid_transfer;
+logic        valid_transfer;
 
-//-----------------------------------------
-//	User logic
-//-----------------------------------------
+assign valid_transfer = ( data_val_i && ( data_mod_i >= 3 ) );
+assign ser_data_o     = ser_data[15];
 
-	// Запрос удовлетворяет условиям по длине передаваемого сообщения
-	assign valid_transfer = (data_val_i && (data_mod_i >= 3));
+always_ff @( posedge clk_i )
+  if( srst_i )
+    busy_o <= 0;
+  else if( valid_transfer )
+    busy_o <= 1;
+  else if( ser_data_cnt == 0 )
+    busy_o <= 0;
 
-	// Назначение выхода сдвигового регистра на выход данных схемы
-	assign ser_data_o = ser_data[15];
+always_ff @( posedge clk_i )
+  if( srst_i )
+    ser_data_cnt <= 0;
+  else if( valid_transfer )
+    ser_data_cnt <= data_mod_i - 1;
+  else if( ser_data_cnt !== 0 )
+    ser_data_cnt <= ser_data_cnt - 1;
 
-	always_ff @(posedge clk_i)begin
-		if(srst_i)begin
-			busy_o <= 0;
-		end else begin
-			if(valid_transfer)begin
-				busy_o <= 1;
-			end else if(ser_data_cnt == 0)begin
-				busy_o <= 0;
-			end
-		end
-	end
+always_ff @( posedge clk_i )
+  if( srst_i )
+    ser_data <= 0;
+  else if( valid_transfer )
+    ser_data <= data_i;
+  else if( ser_data_cnt !== 0 )
+    ser_data <= ser_data << 1;
 
-	always_ff @(posedge clk_i)begin
-		if(srst_i) begin
-			ser_data_cnt <= 0;
-		end else begin
-			if(valid_transfer)begin
-				ser_data_cnt <= data_mod_i - 1;
-			end else if(ser_data_cnt !== 0)begin
-				ser_data_cnt <= ser_data_cnt - 1;
-			end
-		end
-	end
-
-	always_ff @(posedge clk_i)begin
-		if(srst_i) begin
-			ser_data <= 0;
-		end else begin
-			if(valid_transfer)begin
-				ser_data <= data_i;
-			end else if(ser_data_cnt !== 0)begin
-				ser_data <= ser_data << 1;
-			end
-		end
-	end
-
-	always_ff @(posedge clk_i)begin
-		if(srst_i) begin
-			ser_data_val_o <= 0;
-		end else begin
-			if(valid_transfer)begin
-				ser_data_val_o <= 1;
-			end else if(ser_data_cnt == 0)begin
-				ser_data_val_o <= 0;
-			end
-		end
-	end
+always_ff @( posedge clk_i )
+  if( srst_i )
+    ser_data_val_o <= 0;
+  else if( valid_transfer )
+    ser_data_val_o <= 1;
+  else if( ser_data_cnt == 0 )
+    ser_data_val_o <= 0;
 
 endmodule : serializer
--- tb/serializer_tb.sv
+++ tb/serializer_tb.sv
@@ -2,63 +2,55 @@
 
 module serializer_tb;
 
-	bit clk_i = 0;
-	bit srst_i = 0;
+bit clk_i  = 0;
+bit srst_i = 0;
 
-	always #5 clk_i = ~clk_i;
+logic        data_val_i;
+logic [3:0]  data_mod_i;
+logic [15:0] data_i;
 
-	// interconnect variables declaration
-	logic 		 data_val_i;
-	logic [3:0]  data_mod_i;
-	logic [15:0] data_i;
+logic        ser_data_o;
+logic        ser_data_val_o;
 
-	logic 		 ser_data_o;
-	logic 		 ser_data_val_o;
-	logic 		 busy_o;
+logic        busy_o;
 
-	// Module instantiation
-	serializer DUT(
-		.clk_i(clk_i),
-		.srst_i(srst_i),
-		.*
-	);
+serializer DUT(
+  .clk_i  ( clk_i  ),
+  .srst_i ( srst_i ),
+  .*
+);
 
-	// Task for sending message
-	task automatic send_msg(input logic[3:0] data_length);
+task automatic send_msg( input logic[3:0] data_length );
+  @( posedge clk_i );
+  data_mod_i = data_length;
+  data_i     = $urandom;
+  data_val_i = 1;
+  @( posedge clk_i );
+  data_val_i = 0;
+  @( posedge clk_i );
+  wait( !busy_o );
+endtask
 
-		@(posedge clk_i);
-			data_mod_i = data_length;
-			data_i = $urandom;
-			data_val_i = 1;
-		@(posedge clk_i);
-			data_val_i = 0;
-		@(posedge clk_i);
-			wait(!busy_o);
+always #5 clk_i = ~clk_i;
 
-	endtask
+initial begin
+  data_val_i = 0;
+  data_mod_i = 0;
+  data_i     = 0;
 
-	// Test programm
-	initial begin
+  #10;
+  @( posedge clk_i );
+  srst_i = 1;
+  @( posedge clk_i );
+  srst_i = 0;
 
-		// inputs initialization
-		data_val_i = 0;
-		data_mod_i = 0;
-		data_i = 0;
+  #50;
 
-		#10;
-		@(posedge clk_i);
-			srst_i = 1;
-		@(posedge clk_i);
-			srst_i = 0;
+  for( int i = 0; i < 16; i++ )
+    send_msg( i );
 
-		#50;
-
-		for(int i = 0; i < 16; i++)begin
-			send_msg(i);
-		end
-
-		#10;
-		$stop();
-	end
+  #10;
+  $stop();
+end
 
 endmodule : serializer_tb
